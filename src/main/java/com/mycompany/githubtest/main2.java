ead90ce91aa0044f47bcbd3aa16838c99d19479d
liuwenjun95 <liuwenjun95@localhost>
2018-11-19 08:36:22 +0700

    Merge origin/master
    Conflicts:
    	src/main/java/com/mycompany/githubtest/main.java

---
+++ src/main/java/com/mycompany/githubtest/main2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.githubtest;
+
+/**
+ *
+ * @author liuwenjun95
+ */
+public class main2 {
+    public static void main(String[] args) {
+        System.out.println("abc");
+    }
+}
